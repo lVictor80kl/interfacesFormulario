e298d2809721f80b61f1a91db936075ef7b41e5f
MoisesLondo <moiseslondo16@gmail.com>
2025-02-25 16:33:43 -0400

    change

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,16 @@ import authRoutes from "./routes/authRoutes"
 import formRoutes from "./routes/formRoutes"
 import colorRoutes from "./routes/colorRoutes"
 import cors from "cors"
+import path from 'path';
+
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public'));
+
+app.use('/uploads', express.static('uploads'));
 
 app.use("/auth",authRoutes)
 app.use("/forms",formRoutes)
